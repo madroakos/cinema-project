74c0f893786c92bc5b4c843adcccf14da7ed1225
AMADRO <asd@example.com>
2024-10-18 14:57:38 +0200

    make function for retrieving showtime by id

--- prisma/actions.ts
+++ prisma/actions.ts
@@ -11,3 +11,17 @@ export async function getAllMovies() {
   })
   return movies
 }
+
+export async function getShowtimeByID(showtimeID: string) {
+  const prisma = new PrismaClient()
+  const showtime = await prisma.showtimes.findUnique({
+    where: {
+      showtime_id: Number(showtimeID),
+    },
+    include: {
+      Movies: true,
+      Screens: true,
+    },
+  })
+  return showtime
+}
