89ee037d646055c272a20a3c674cfceeccb41c72
AMADRO <asd@example.com>
2024-10-21 08:42:51 +0200

    basic logic for showing seats, not complete

--- app/reservation/[showtimeID]/page.tsx
+++ app/reservation/[showtimeID]/page.tsx
@@ -28,6 +28,24 @@ export default async function ReservationPage({ params }: { params: { showtimeID
                 <p className="text-xl">{formattedDate}, {showtime.Screens.screen_number}. terem</p>
                 <p>{showtime.language} | {showtime.dimension} {hasSubtitle()}</p>
             </div>
+            <div>
+                <div className="flex flex-col gap-1 mt-4">
+                    <div className="flex flex-col self-center gap-1">
+                    {Array.from({ length: showtime.Screens.rows }).map((_, rowIndex) => (
+                        <div key={rowIndex} className="flex max-w-screen">
+                        {Array.from({ length: showtime.Screens.columns }).map((_, colIndex) => (
+                            <button
+                            className="m-0.5 w-3 h-3 bg-black rounded-sm"
+                            key={`${rowIndex}-${colIndex}`}
+                            //   onClick={() => handleReservation(rowIndex + 1, colIndex + 1)}
+                            >
+                            </button>
+                        ))}
+                        </div>
+                    ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
